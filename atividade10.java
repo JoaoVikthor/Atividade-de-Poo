36dc1be216b5335547d58a81f760c0a96312e7aa
Vikthor <dev.joao.victor@outlook.com>
2026-09-24 22:04:04 -0300

    Adcionados as atividades faltantes de poo

---
+++ atividade10.java
@@ -0,0 +1,12 @@
+public class atividade10 {
+
+    public static void main(String[] args) {
+
+        int contadorCliente = 0;
+
+        for (int i = 0; i < 5; i++) {
+            contadorCliente++;
+            System.out.println("Chegada de cliente numero: " + contadorCliente);
+        }
+    }
+}
